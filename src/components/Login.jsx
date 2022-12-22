ee9cceb5b47e5da04559dc64b0f098a5f9677c9a
karthikeyan k <105141303+KarthikeyanKanniappan@users.noreply.github.com>
2022-12-22 16:03:34 +0530

    homepage_commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 import {
   FormControl,
   FormLabel,
@@ -8,39 +10,101 @@ import {
   Button,
   VStack,
 } from "@chakra-ui/react";
+import { env } from "../config.js";
+import axios from "axios";
+
 const Login = () => {
   const [show, setShow] = useState(false);
+  const navigate = useNavigate();
   let handleClick = () => {
     setShow(!show);
   };
+
+  let formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      let errors = {};
+      if (values.email === "") {
+        errors.email = "Please enter your email  ";
+      }
+      if (values.password === "") {
+        errors.password = "Password should not be empty";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+      try {
+        if (values.email && values.password) {
+          let loginData = await axios.post(`${env.api}/users/login`, values);
+          if (loginData.status) {
+            window.localStorage.setItem("app-token", loginData.data.token);
+            window.localStorage.setItem("User", loginData.data.result.name);
+            alert("signed in successfully");
+            navigate("/chats");
+          }
+        }
+      } catch (err) {
+        alert(err.response.data.message);
+      }
+    },
+  });
   return (
-    <VStack spacing="5px" color="black">
-      <FormControl>
-        <FormLabel color="black">
-          Email
-          <Input placeholder="Enter your Email"></Input>
-        </FormLabel>
-      </FormControl>
-      <FormControl>
-        <FormLabel color="black">
-          Password
-          <InputGroup>
-            <Input color="black" placeholder="Enter your password" />
-            <InputRightElement>
-              <Button color="black" h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? "Hide" : "Show"}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormLabel>
-      </FormControl>
-      <Button colorScheme="blue" width="100%" style={{ marginTop: 15 }}>
-        Sign In
-      </Button>
-      <Button colorScheme="red" width="100%" style={{ marginTop: 15 }}>
-        Guest Credentials
-      </Button>
-    </VStack>
+    <form onSubmit={formik.handleSubmit}>
+      <VStack spacing="5px" color="black">
+        <FormControl>
+          <FormLabel color="black">
+            Email
+            <Input
+              placeholder="Enter your Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              name="email"
+            ></Input>
+            <span style={{ color: "red" }}>{formik.errors.email}</span>
+          </FormLabel>
+        </FormControl>
+        <FormControl>
+          <FormLabel color="black">
+            Password
+            <InputGroup>
+              <Input
+                color="black"
+                type={show ? "text" : "password"}
+                placeholder="Enter your password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                name="password"
+              />
+              <InputRightElement>
+                <Button
+                  color="black"
+                  h="1.75rem"
+                  size="sm"
+                  onClick={handleClick}
+                >
+                  {show ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <span style={{ color: "red" }}>{formik.errors.password}</span>
+          </FormLabel>
+        </FormControl>
+        <Button
+          type="submit"
+          colorScheme="blue"
+          width="100%"
+          style={{ marginTop: 15 }}
+        >
+          Sign In
+        </Button>
+        <Button colorScheme="red" width="100%" style={{ marginTop: 15 }}>
+          Guest Credentials
+        </Button>
+      </VStack>
+    </form>
   );
 };
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -7,63 +7,162 @@ import {
   Button,
   VStack,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import { env } from "../config.js";
+import axios from "axios";
 
 const SignUp = () => {
   const [show, setShow] = useState(false);
+  const [shows, setShows] = useState(false);
+  const navigate = useNavigate();
+
   let handleClick = () => {
     setShow(!show);
   };
+
+  let handleClick1 = () => {
+    setShows(!shows);
+  };
+
+  let formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      pic: "",
+    },
+
+    validate: (values) => {
+      let errors = {};
+      if (values.name === "") {
+        errors.name = "Please enter your name  ";
+      }
+      if (values.email === "") {
+        errors.email = "Please enter your email  ";
+      }
+      if (values.password === "") {
+        errors.password = "Password should not be empty";
+      }
+      return errors;
+    },
+
+    onSubmit: async (values) => {
+      if (values.email && values.password) {
+        try {
+          if (values.email && values.password) {
+            let loginData = await axios.post(
+              `${env.api}/users/register`,
+              values
+            );
+            if (loginData.status) {
+              alert("User Created");
+            }
+          }
+        } catch (err) {
+          alert(err.response.data.message);
+        }
+      }
+    },
+  });
+
   return (
-    <VStack spacing="5px">
-      <FormControl>
-        <FormLabel color="black">
-          Name
-          <Input placeholder="Enter your name"></Input>
-        </FormLabel>
-      </FormControl>
-      <FormControl>
-        <FormLabel color="black">
-          Email
-          <Input placeholder="Enter your Email"></Input>
-        </FormLabel>
-      </FormControl>
-      <FormControl>
-        <FormLabel color="black">
-          Password
-          <InputGroup>
-            <Input color="black" placeholder="Enter your password" />
-            <InputRightElement>
-              <Button color="black" h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? "Hide" : "Show"}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormLabel>
-      </FormControl>
-      <FormControl>
-        <FormLabel color="black">
-          Confirm Password
-          <InputGroup>
-            <Input color="black" placeholder="Enter your Confirm password" />
-            <InputRightElement>
-              <Button color="black" h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? "Hide" : "Show"}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormLabel>
-      </FormControl>
-      <FormControl>
-        <FormLabel color="black">
-          Upload your Picture
-          <Input placeholder="Add Image"></Input>
-        </FormLabel>
-      </FormControl>
-      <Button colorScheme="blue" width="100%" style={{ marginTop: 15 }}>
-        Sign Up
-      </Button>
-    </VStack>
+    <form onSubmit={formik.handleSubmit}>
+      <VStack spacing="5px">
+        <FormControl>
+          <FormLabel color="black">
+            Name
+            <Input
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              name="name"
+              placeholder="Enter your name"
+            ></Input>
+            <span style={{ color: "red" }}>{formik.errors.name}</span>
+          </FormLabel>
+        </FormControl>
+        <FormControl>
+          <FormLabel color="black">
+            Email
+            <Input
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              name="email"
+              placeholder="Enter your Email"
+            ></Input>
+            <span style={{ color: "red" }}>{formik.errors.email}</span>
+          </FormLabel>
+        </FormControl>
+        <FormControl>
+          <FormLabel color="black">
+            Password
+            <InputGroup>
+              <Input
+                type={show ? "text" : "password"}
+                color="black"
+                placeholder="Enter your password"
+              />
+              <InputRightElement>
+                <Button
+                  color="black"
+                  h="1.75rem"
+                  size="sm"
+                  onClick={handleClick}
+                >
+                  {show ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormLabel>
+        </FormControl>
+        <FormControl>
+          <FormLabel color="black">
+            Confirm Password
+            <InputGroup>
+              <Input
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                name="password"
+                type={shows ? "text" : "password"}
+                color="black"
+                placeholder="Enter your Confirm password"
+              />
+              <InputRightElement>
+                <Button
+                  color="black"
+                  h="1.75rem"
+                  size="sm"
+                  onClick={handleClick1}
+                >
+                  {shows ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <span style={{ color: "red" }}>{formik.errors.password}</span>
+          </FormLabel>
+        </FormControl>
+        <FormControl>
+          <FormLabel color="black">
+            Upload your Picture
+            <Input
+              value={formik.values.pic}
+              onChange={formik.handleChange}
+              name="pic"
+              placeholder="Add Image"
+            ></Input>
+          </FormLabel>
+        </FormControl>
+        <Button
+          type="submit"
+          colorScheme="blue"
+          width="100%"
+          style={{ marginTop: 15 }}
+        >
+          Sign Up
+        </Button>
+      </VStack>
+    </form>
   );
 };
 
